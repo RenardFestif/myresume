55f4aaba02526476d8adc2332ae6799ed5695d4f
Babouche <jub.saadi@gmail.com>
2020-01-29 16:16:58 +0100

    Updates

---
+++ precache-manifest.915741bacd4e2bde97313966c17738ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079816cb7ea8079d0ea241b8af6d1715",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "be7028d789a93903f5c2",
+    "url": "/myresume/static/css/2.aa25f744.chunk.css"
+  },
+  {
+    "revision": "02faf4fd3fa05d5e6917",
+    "url": "/myresume/static/css/main.1a73fb2d.chunk.css"
+  },
+  {
+    "revision": "be7028d789a93903f5c2",
+    "url": "/myresume/static/js/2.3ace019b.chunk.js"
+  },
+  {
+    "revision": "ef0359148dfd247c2fff169c4e7f3571",
+    "url": "/myresume/static/js/2.3ace019b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02faf4fd3fa05d5e6917",
+    "url": "/myresume/static/js/main.a0ab9224.chunk.js"
+  },
+  {
+    "revision": "fb19e715b8cd15de2f84",
+    "url": "/myresume/static/js/runtime-main.eaff96b8.js"
+  }
+]);
